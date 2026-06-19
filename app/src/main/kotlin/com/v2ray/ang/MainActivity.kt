00ecf25867c108016599cf77099e503571d23978
root <root@localhost>
2026-06-19 20:37:11 +0700

    Cloud Build Request - 2026-06-19 20:37:11

---
+++ app/src/main/kotlin/com/v2ray/ang/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.v2ray.ang
+
+import android.app.Activity
+import android.os.Bundle
+import com.v2ray.ang.R
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
